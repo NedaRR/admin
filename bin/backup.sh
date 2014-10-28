34caa35909c7a7063d3ceb6bee02dcd39f9fa9d6
Jon Pipitone <jon.pipitone@gmail.com>
2014-10-27 20:33:29 -0400

    Updated backup script to treat source folders as mountpoints.
    Previously we had  so that we could sync from a subfolder in
    a mountpoint to a destination. For better or worse, I've simplified the script so that
    the  function just syncs between mount points and a destination. Hopefully
    this makes the script more readable.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Backup our servers
+# Backup all the things
 # This script expects to be run as root :-)
 
 # set path for cron-y goodness
@@ -10,34 +10,33 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 excludes="--exclude=.cache --exclude=Cache --exclude='.Trash*'"
 excludes="$excludes --exclude=.thumbnails --exclude=Trash"
-rsync_opts="-aH --delete $excludes"   # set to -n for a dry run
+rsync_opts="-aH --delete $excludes"   # add -n for a dry run
 
 function log { echo "$1"; }
 function die { echo "$1"; exit 1; }
 function sync { 
-  mnt=$1    # mount holding src
-  src=$2    # source folder to rsync (trailing slashes are important!)
-  dest=$3   # destination folder 
+  src=$1    # source folder to rsync (trailing slashes are important!)
+  dest=$2   # destination folder 
   log "Backup $src to $dest"
-  mountpoint -q $mnt || (mount $mnt || die "Failed to mount $mnt. Skipping.")
+  mountpoint -q $src || (mount $src || die "Failed to mount $src. Skipping.")
   rsync $rsync_opts $src $dest
 }
   
 log "Mounting ZFS"
 zfs mount -a    || die "Failed to mount ZFS datasets"
 
-# note trailing slashes
-sync /archive        /archive/data    /tank/archive
-sync /archive        /archive/spins   /tank/archive
-sync /home           /home            /tank
-sync /projects       /projects        /tank
-sync /quarantine     /quarantine      /tank
-sync /opt/quarantine /opt/quarantine/ /tank/quarantine-nouveau
+# note trailing slashes on source folders. rsync treats this as "copy everything
+# from inside the folder, but not the folder itself"
+sync /archive         /tank
+sync /home            /tank
+sync /projects        /tank
+sync /quarantine      /tank
+sync /opt/quarantine/ /tank/quarantine-nouveau
 
 log "Taking a ZFS snapshot of tank"
 /usr/local/bin/zsnap --keep=8 --prefix=daily- tank
 
-log "Backup /home to tigrsrv:/pool/home and taking ZFS snapshot"
+log "Backup /home to tigrsrv:/pool/home and take ZFS snapshot"
 sudo -u localroot ssh tigrsrv \
   "(mountpoint -q /pool/home || sudo zfs mount pool/home) &&
    sudo rsync $rsync_opts /home/ /pool/home &&
