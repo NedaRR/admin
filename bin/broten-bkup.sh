5123c4ed470ac0ff136bd7b11fa9b8f6f7a50ca1
Jon Pipitone <jon@pipitone.ca>
2015-07-24 17:04:19 -0400

    Backup of David's project folder has changed.

--- bin/broten-bkup.sh
+++ bin/broten-bkup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # trailing slash on /projects/david/ says "copy the contents"
-rsync -a --delete /projects/david/ /external/david/projects/
+rsync -a --delete /projects/david/ /alumni/david/projects/
