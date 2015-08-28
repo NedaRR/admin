782f0928b3ec0bb5b227383a983e4436b0f6c651
Jon Pipitone <jon.pipitone@gmail.com>
2015-08-28 14:58:17 -0400

    Moved /projects/david backup to /alumni

--- bin/broten-bkup.sh
+++ bin/broten-bkup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # trailing slash on /projects/david/ says "copy the contents"
-rsync -a --delete /projects/david/ /alumni/david/projects/
+rsync -a --delete /projects/david/ /mnt/alumni/david/projects/
